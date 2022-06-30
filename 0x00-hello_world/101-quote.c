5a56bfda4ea4e9f46e9d154eccaf5015b83d27a4
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-06-30 08:59:12 -0700

    Add pointer and fix style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
 
 int main(void)
 {
-	char *c= "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
+	char *c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fputs(c, stdout);
 	return (1);
 }
