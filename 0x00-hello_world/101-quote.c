364fd46ca9aa523027ea80fc41cfb412a43e670f
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-06-30 08:53:08 -0700

    Print without using printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main-Entry point
+ *Return:Always 0
+ */
+
+int main(void)
+{
+	char *c= "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
+	fputs(c, stdout);
+	return (1);
+}
