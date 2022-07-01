234ce187e9943b52f2f406347547beb4f3e76fd6
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-01 14:24:18 -0700

    Place holder till solution

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,3 @@
-/*
- * File: 100-print_comb3.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
@@ -23,7 +18,7 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
-				continue;
+			continue;
 			
 			putchar(',');
 			putchar(' ');
