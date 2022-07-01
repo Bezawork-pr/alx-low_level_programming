02b27a78260a1a150481c576027094ddfaf58e50
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-01 12:46:30 -0700

    rite a program that prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char h;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		putchar(i);
+	}
+	for (h = 'a'; h <= 'f'; h++)
+	{
+		putchar(h);
+	}
+		putchar('\n');
+	return (0);
+}
