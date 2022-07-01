58c07e1eaf9b2287e0dc9b458fc04fddb89c547f
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-01 06:23:54 -0700

    Fix betty errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -11,13 +14,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-	if (n > 0){
-	printf("%d is positive\n",n);
-	}else if (n == 0){
-	printf("%d is zero\n",n);
-	}else{
-	printf("%d is negative\n",n);
-	}
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is directory will contain if else and loops related assignments
