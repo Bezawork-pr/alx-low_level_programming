200b4d9257ddefe87935d11ed2cec2de838a9c8c
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-05 05:21:45 -0700

    Print _putchar()

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main-Entry point
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int i = _putchar('c');
+
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
