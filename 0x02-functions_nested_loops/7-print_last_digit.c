b90b7d47c5ffb477160eb95045044350a42bb3d7
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-05 16:12:47 -0700

    Return last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit -a function that prints the last digit of a number
+*
+* @n: takes signed integer
+*
+* Return: The last digit
+*/
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	return (ld);
+}
