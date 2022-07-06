9a5b935735787b357a7bf97ede95a6c0dc541261
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-06 04:31:41 -0700

    Add curly braces for body of for loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,20 +18,24 @@ void print_to_98(int n)
 			if (n <= 9)
 			{
 				_putchar(n + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	else
 	{
 		for (; n > 98 ; n--)
-			if (n > 98)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 }
