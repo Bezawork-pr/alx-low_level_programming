7fa2b9fd6d25b1c553af688d34a72218fa830795
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-05 23:42:42 -0700

    Fix code error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,20 +7,22 @@
 */
 void times_table(void)
 {
-	int row = 0;
 	int colon = 0;
 
 	while (colon < 10)
 	{
+		int row = 0;
 		while (row < 10)
 		{
 			int result = colon * row;
 
 			_putchar(result + '0');
 			_putchar(',');
+			_putchar('\n');
 			row++;
 		}
 		colon++;
+		_putchar(' ');
 
 	}
 
