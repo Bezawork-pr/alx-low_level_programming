0926f6eb98fbca4d0e12c1e1d99b656e7e9b1707
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-06 10:01:02 -0700

    redo 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
-* main - checks if int is positive or negative
-*
-* Return: int
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
