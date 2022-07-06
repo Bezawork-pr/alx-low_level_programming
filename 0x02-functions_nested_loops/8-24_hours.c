a63aad4633ab2472382c0585972bb181ae59616a
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-05 17:01:07 -0700

    print minute and hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* jack_bauer - a function that prints every minute of the day
+*
+* Return: void
+*/
+void jack_bauer(void)
+{
+	int m = 0;
+	int h = 0;
+
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+
+	}
+}
