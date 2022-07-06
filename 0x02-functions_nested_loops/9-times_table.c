c967e8bd96a5f80df83758f383dba70eda9968db
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-06 00:59:24 -0700

    Debug for problem

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,15 +20,14 @@ void times_table(void)
 			if (result <= 9)
 			{
 				_putchar(result + '0');
+				_putchar(',');
+				_putchar(' ');
 			} 
 			else
 			{
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 			row++;
 		}
 		_putchar('\n');
