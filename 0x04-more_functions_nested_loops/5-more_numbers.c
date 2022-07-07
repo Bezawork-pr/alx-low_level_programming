315863db5d99882af2eb4270ff29d1d07cacf452
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-07 09:49:20 -0700

    Remove else and  braces

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,9 @@ void more_numbers(void)
 
 		for (; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
-			else
-			{
+			if (j > 9)
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
-			}
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
