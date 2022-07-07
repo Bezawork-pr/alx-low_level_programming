62b9dc79edf05625f4cd445e9f761aa2e46f3c52
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-07 09:33:32 -0700

    Print from 1 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - Print (0 - 14) * 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 10; i++)
+	{
+		int j = 0;
+
+		for (; j <= 14; j++)
+		{
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
