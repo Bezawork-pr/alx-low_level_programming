e77f25f743468d1fc6bf8e58dd807d8b5e7e760b
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-07 03:18:45 -0700

    Add new function to check date validity

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * valmondat- checks if days in months are valid
+ *
+ * @month: takes int
+ *
+ * @date: takes int
+ *
+ * Return: int
+ */ 
+int valmondat(int month, int day)
+{
+	if(month == 4 || month == 6 || month == 9 || month == 11)
+	{
+		if ((day <= 30) && (day > 0))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else if (month == 2)
+	{
+		if ((day <=29) && (day > 0))
+		{
+			return (1);
+		}
+		else 
+		{
+			return (0);
+		}
+	}
+	else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 12 || month == 10) 
+	{
+		if ((day <= 31) && (day > 0))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+				       
+	}
+	return (0);
+}
 /**
 * * print_remaining_days - takes a date and prints how many days are
 * * left in the year, taking leap years into account
@@ -12,7 +58,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0) && (valmondat(month, day) == 1))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -24,7 +70,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if((day >= 1) && (day <= 31) && ((month >= 1) && (month <= 12)))
+		if (valmondat(month, day) == 1)
 		{
 			if (month == 2 && day == 60)
 			{
