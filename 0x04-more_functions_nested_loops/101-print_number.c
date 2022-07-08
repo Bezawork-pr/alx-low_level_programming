685d98d81c7eeee83f7aef2d6d4274bb673f6cfa
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-08 10:56:07 -0700

    update file to file that passed most checks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,28 +11,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
-	if ((n <= 9) && (n >= 0))
-	{
-		_putchar((n % 10) + '0');
-	}
-	else if ((n > 9) && (n < 100))
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 100) && (n < 1000))
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
