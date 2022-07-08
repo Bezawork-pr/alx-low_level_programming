593b2b678e6805902e8064b3311ec504d4ad0fe4
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-08 09:57:06 -0700

    fix code in else

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -31,8 +31,8 @@ void print_number(int n)
 	else
 	{
 		_putchar((n / 1000) + '0');
-		_putchar((n / 100) + '0');
 		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
 		_putchar((n % 10) + '0');
 	}
 }
