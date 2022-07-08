b8a6e5de19a32dfa5df8a4d8450098edd5d7b17d
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-07 19:35:22 -0700

    Add appropraite space before # to make the triangle look like example

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *print_triangle - prints triangle
+ *
+ * @size: Takes int
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i = 0;
+
+		for (; i <= size; i++)
+		{
+			int m = 0;
+			int k = 0;
+
+			for (; k < (size - i); k++)
+			{
+				_putchar(' ');
+			}
+
+			for (; m < i; m++)
+			{
+				_putchar('#');
+			}
+		       _putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
