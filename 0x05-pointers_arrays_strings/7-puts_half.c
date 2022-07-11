f3d8f15e0f7cabb4d83d0110ed78d0a5ef023a4a
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-11 16:12:48 -0700

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string
+ *
+ * @str : takes string
+ *
+ * Return : void
+ */
+void puts_half(char *str)
+{
+	int count = 0;
+	int i = 0;
+	int m;
+
+	for (; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		for (m = (count / 2); m < count; m++)
+		{
+			_putchar(str[m]);
+		}
+	}
+	else
+	{
+		for (m = ((count - 1) / 2); m < count; m++)
+		{
+			_putchar(str[m]);
+		}
+	}
+}
