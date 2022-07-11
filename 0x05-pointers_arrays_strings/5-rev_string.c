2ffb1831788323b6dfdbda4042a40da6b9720421
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-11 14:47:54 -0700

    Try answer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverse a string
+ *
+ * @s: takes strng
+ *
+ * Return: Void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int count = 0;
+	int m;
+	char rev[9]; 
+
+	for (; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	count--;
+	for (m = 0; count >= 0; count--)
+	{
+		rev[m] = s[count];
+		m++;
+	}
+	printf("%s", rev);
+}
