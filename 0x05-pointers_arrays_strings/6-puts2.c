84f0541baeaf0aab8b7d743a402b3c3e71fdcd67
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-11 15:22:43 -0700

    Print every other number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 -prints every other character of a string, starting with the first character
+ *
+ * @str: takes string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for(; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
