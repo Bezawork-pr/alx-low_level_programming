e3c70c6eca125f0ca39656c481a6e6689121cd39
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-12 03:47:32 -0700

    add backslash 0 to let end of string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * rev_string - reverse a string
+ * rev_string - reverse a strinG
  *
  * @s: takes strng
  *
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	int i = 0;
 	int count = 0;
 	int m;
-	char rev[9];
+	char rev[1000];
 
 	for (; s[i] != '\0'; i++)
 	{
@@ -23,6 +23,7 @@ void rev_string(char *s)
 	for (m = 0; count >= 0; count--)
 	{
 		rev[m] = s[count];
+		rev[m + 1] = '\0';
 		m++;
 	}
 	strcpy(s, rev);
