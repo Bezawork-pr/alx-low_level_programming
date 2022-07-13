69ee58a79a676d798983577ebb6c07f41bff2f90
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-13 10:59:36 -0700

    Pass checker fail

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,9 +13,11 @@ int _strcmp(char *s1, char *s2)
 	int i, comp;
 
 	i = comp = 0;
+
+
 	for (; (s1[i] != '\0') && (comp == 0); i++)
 	{
-		comp =s1[i] -s2[i];  
+		comp = s1[i] - s2[i];
 	}
 	return (comp);
 }
