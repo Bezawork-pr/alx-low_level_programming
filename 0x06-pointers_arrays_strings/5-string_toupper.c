1410b6513419adc5e71d371d60a27c70fed4e0bc
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-13 08:11:14 -0700

    Change to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes string to uppercase
+ *
+ * @ip: takes string
+ *
+ * Return: Point to string
+ */
+char *string_toupper(char *ip)
+{
+	int i = 0;
+
+	for (; ip[i] != '\0'; i++)
+	{
+		if ((ip[i] > 'a') && (ip[i] < 'z'))
+		{
+			ip[i] = ip[i] - 32;
+		}
+	}
+	return (ip);
+}
