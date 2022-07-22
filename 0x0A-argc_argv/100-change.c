c3325f458ec6d4fd74ecf74b63af55203412dd1d
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-22 10:00:02 -0700

    Make change to coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * @argc: takes int
+ *
+ * @argc: Takes string
+ *
+ * Return: return int
+ */
+int main(int argc, char *argv[]) 
+{
+	int coin = atoi(argv[1]);
+	int count = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (coin < 0)
+	{
+		printf("%d\n", coin);
+	}
+	while (coin > 0)
+	{
+		if (coin >= 25)
+		{
+			coin = coin - 25;
+			count++;
+		}
+		else if ((coin < 25) && (coin >= 10))
+		{
+			coin = coin - 10;
+			count++;
+		}
+		else if ((coin < 10) && (coin >= 5))
+		{
+			coin = coin -5;
+			count++;
+		}
+		else if ((coin < 5) && (coin >= 2))
+		{
+			coin = coin - 2;
+			count ++;
+		}
+		else if ((coin < 2) && (coin >= 1))
+		{
+			coin = coin - 1;
+			count++;
+		}
+
+	}
+	printf("%d\n", count);
+	return (0);
+}
