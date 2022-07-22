0d712d58ce73ec546f24f05605015014e61a99f3
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-22 04:13:32 -0700

    Add Description for function

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Name of function
+ *
+ * @argc: Takes int
+ *
+ * @argv: Takes string
+ *
+ * Return: int
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
