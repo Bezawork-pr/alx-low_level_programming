4b40bcb4bbd2f9a7f42590203eeebe1f1c78115f
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-25 07:05:08 -0700

    Copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - makes a copy
+ *
+ * @str: Takes a string
+ *
+ * Return: A pointer
+ */
+char *_strdup(char *str)
+{
+	char *cp;
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	cp = malloc(sizeof(*str));
+	for (; str[i] != '\0'; i++)
+	{
+		cp[i] = str[i];
+	}
+	return (cp);
+
+}
