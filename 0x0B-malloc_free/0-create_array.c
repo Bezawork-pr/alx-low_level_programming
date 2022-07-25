9f11dee1a0f9b4c17f941f838608e5f5e81f62c6
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-25 05:37:28 -0700

    Write a function that creates array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array
+ *
+ * @size: Takes unsigned int
+ *
+ * @c : Takes character
+ *
+ * Return: Pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *alsize;
+	unsigned int i = 0;
+
+	alsize = malloc(sizeof(*alsize) * size);
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	while (i < size)
+	{
+		alsize[i] = c;
+		i++;
+	}
+	if (*alsize == '\0')
+	{
+		return ('\0');
+	}
+	return (alsize);
+
+}
