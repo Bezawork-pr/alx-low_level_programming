5b5e56c468a24831ea2d7377f9102224952029d3
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-26 05:40:27 -0700

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - Free grid
+ *
+ * @grind: Takes double pointer
+ *
+ * @height: Takes int
+ *
+ * Return: Void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
