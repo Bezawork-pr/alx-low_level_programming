844f5c2ea737a3fd875f2f921cc4d0be3b1b51c3
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-28 07:50:48 -0700

    Allocate memory with malloc to create a function like calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - Allocate memory
+ *
+ * @nmemb: member of array that memory is
+ * going to be allocated for
+ *
+ * @size: type size
+ *
+ * Return: void pointer, Null if argument is 0
+ * Null if fail to allocate memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *cal;
+
+	if ((nmemb == '0') || (size == '0'))
+	{
+		return (NULL);
+	}
+	cal = malloc(nmemb * size);
+	if (cal == NULL)
+	{
+		return (NULL);
+	}
+	return (cal);
+}
