0ee058a03c3f9e835c385fbe782e8cc0bcb1a5d2
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-07-28 06:41:02 -0700

    Replace library

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,95 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+/**
+ * len - Finds length
+ *
+ * @s: Takes string to calcluate length
+ *
+ * Return: length of string
+ */
+unsigned int len(char *s)
+{
+	int i = 0, count = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
+/**
+ * _strcpy - copies the string pointed to by src
+ *
+ * @dest: buffer ponter
+ *
+ * @src: string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		dest[i + 1] = '\0';
+	}
+	return (dest);
+}
+/**
+ * _strcat- concats two string
+ *
+ * @dest: Takes string
+ *
+ * @src: takes string
+ *
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int m = 0;
+	int count = 0;
+
+	for (; dest[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (; src[m] != '\0'; m++)
+	{
+		dest[count] = src[m];
+		count++;
+	}
+	return (dest);
+}
+/**
+ * _strncat-concatenates two strings
+ *
+ * @dest: takes string
+ *
+ * @src: takes string
+ *
+ * @n: takes int
+ *
+ * Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, m, count;
+
+	i = m = count = 0;
+	for (; dest[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (; (m < n) && (src[m] != '\0') ; m++)
+	{
+		dest[count] = src[m];
+		count++;
+	}
+	return (dest);
+}
 /**
  * string_nconcat- concatenates two strings
  *
@@ -16,8 +104,8 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int lens1 = strlen(s1);
-	unsigned int lens2 = strlen(s2);
+	unsigned int lens1 = len(s1);
+	unsigned int lens2 = len(s2);
 	unsigned int len;
 	char *cp;
 
@@ -30,23 +118,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 	cp = malloc(sizeof(*s1));
-	cp = strcpy(cp, s1);
+	cp = _strcpy(cp, s1);
 	if (n >= lens2)
 	{
 		len = lens1 + lens2 + 1;
 		s1 = malloc(sizeof(char) * len);
-		s1 = strcpy(s1, cp);
+		s1 = _strcpy(s1, cp);
 	}
 	else
 	{
 		len = lens1 + n + 1;
 		s1 = malloc(sizeof(char) * len);
-		s1 = strcpy(s1, cp);
+		s1 = _strcpy(s1, cp);
 	}
 	if (s1 == NULL)
 	{
 		return (NULL);
 	}
-	strncat(s1, s2, n);
+	_strncat(s1, s2, n);
 	return (s1);
 }
