4c5077f3ee663f001965775f0434c69e347c49e2
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-01 02:50:17 -0700

    init dog struct with init_dog function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ * init_dog - initalizes dog struct
+ *
+ * @d: struct defined based on dog struct
+ *
+ * @name: name
+ *
+ * @age: age
+ *
+ * @owner: owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
