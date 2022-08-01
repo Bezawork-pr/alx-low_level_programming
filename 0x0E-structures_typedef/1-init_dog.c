749dfa6580e777b515e2e09a69bd3a8468fd404b
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-01 02:56:00 -0700

    Add memorry using malloc and handle malloc return

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initalizes dog struct
  *
@@ -14,6 +15,14 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+		{
+			return;
+		}
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
