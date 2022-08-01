2a7861fdaf6d86770008e0ff4540b732639bef33
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-01 03:23:44 -0700

    Print dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - print struct dog
+ *
+ * @d : struct based on dog struct
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "nil";
+	}
+	if (d->age <= 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "nil";
+	}
+	printf("Name: %s\nAge: %6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
