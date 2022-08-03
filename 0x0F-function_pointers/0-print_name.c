fad6cda0839c05ea14f56b3801e70779b082a5e3
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-03 08:38:08 -0700

    handle return if arguments are null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - Prints name
@@ -10,5 +11,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if ((name == NULL) || f == NULL)
+		return;
 	f(name);
 }
