b840e2b89d95247344941d236783bece873e8f59
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-03 01:24:41 -0700

    Execute function on each array memeber

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * array_iterator- Executes function passed on each array element
+ *
+ * @array: array passed
+ *
+ * @size: size of array
+ *
+ * @action: function passed
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
