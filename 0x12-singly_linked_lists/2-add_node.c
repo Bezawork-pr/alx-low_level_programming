fdc47bd13a691080d7b88080e9119ff1a3f03fd5
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-13 08:09:56 -0700

    handle malloc NUll return

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,6 +16,10 @@ list_t *add_node(list_t **head, const char *str)
 	int i = 0;
 
 	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	temp = *head;
 	*head = new;
 	(new->str) = strdup(str);
