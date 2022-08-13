2bcf49f9757e991f1777fa788c5db5f3d94d9ee9
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-13 07:09:29 -0700

    Print before main function execution

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * printbeforemain - prints before main
+ *
+ * Return: void
+ */
+void printbeforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
