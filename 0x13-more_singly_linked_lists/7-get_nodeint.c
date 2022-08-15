43afb5407fd736fb5a091f2348f006caa6c7b1f9
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-15 06:48:59 -0700

    return nth node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get the nth node
+ *
+ * @head: takes head
+ *
+ * @index: nth element
+ *
+ * Return: nth element
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+	unsigned int count = 0;
+
+	temp = head;
+	while (temp->next)
+	{
+		if (count == index)
+		{
+			return (temp);
+		}
+		temp = temp->next;
+		count++;
+	}
+	return (NULL);
+}
