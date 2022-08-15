10304216cacf4f4d5038148b36fa979e9ebe3a71
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-15 04:35:50 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - frees list
+ *
+ * @head: recevies a struct
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
