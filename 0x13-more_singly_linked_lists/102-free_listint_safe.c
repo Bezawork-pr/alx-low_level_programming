3c9ca04a41989d1c317c194e56fe68fd916e0b68
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-16 08:47:19 -0700

    First draft soln

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,1 +1,25 @@
 #include "lists.h"
+/**
+ * free_listint_safe - frees list
+ *
+ * @h: takes head of linked list
+ *
+ * Return: the size of list freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp;
+	size_t count = 0;
+
+	if (*h == NULL)
+		return (count);
+	temp = *h;
+	while (*h)
+	{
+		temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		count++;
+	}
+	return (count);
+}
