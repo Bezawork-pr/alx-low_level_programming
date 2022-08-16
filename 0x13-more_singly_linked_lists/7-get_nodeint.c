1b361618ffd6da8281a2a8037aac6dad65c8cb37
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-16 07:59:59 -0700

    Handle when head is NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int count = 0;
 
+	if (!head)
+		return (NULL);
+
+	if (index == 0)
+		return (head);
+
 	temp = head;
 	while (temp->next)
 	{
