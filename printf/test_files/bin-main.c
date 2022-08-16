cd7e8487bea076a2fc0c00f90dbbb5419db6c709
Bezawork-pr <107026397+Bezawork-pr@users.noreply.github.com>
2022-08-16 14:59:46 +0300

    Delete printf directory

--- printf/test_files/bin-main.c
+++
@@ -1,7 +0,0 @@
-#include "../main.h"
-int main()
-{
-	    int n = 17;
-	        decToBinary(n);
-		    return 0;
-}
--- printf/test_files/uint-main.c
+++
@@ -1,8 +0,0 @@
-#include "../main.h"
-int main()
-{
-	    unsigned int n = 12045;
-	        _print_unsignedint(n);
-		_putchar('\n');
-		    return (0);
-}
