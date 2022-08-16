3d74e9c62f0e6e51a50b4f80598437e41f7b0b0c
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-16 07:27:15 -0700

    Add description

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,23 @@
 #include "lists.h"
+/**
+ * free_listint2 - free list
+ *
+ * @head: head of linked list
+ *
+ * Return: void
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if ((*head == NULL) || (head == NULL))
+	if (*head == NULL)
 		return;
+
+	temp = *head;
 	while (*head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
-	*head = 0;
 }
