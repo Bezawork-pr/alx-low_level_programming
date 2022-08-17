15a5f740e2b2755ccbeed5cb41f1a7729b87bc32
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-17 02:38:37 -0700

    Handle if idx > node count

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -48,6 +48,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		count++;
 	}
+	if (idx > count)
+		return (NULL);
+
 	prev->next = new;
 	prev = prev->next;
 	new->next = temp;
