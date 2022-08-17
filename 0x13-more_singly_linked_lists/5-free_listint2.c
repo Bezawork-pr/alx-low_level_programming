008bfabab518dbb90db718c1fd089dfdbbaef328
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-17 00:51:18 -0700

    Work with new file to check undefined behaviour

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,22 @@
 #include "lists.h"
 /**
- * free_listint2 - frees a list
+ * free_listint2 - free list
  *
- * @head: head of node
+ * @head: head node of linked list
  *
- * Return void
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if ((*head == NULL) || (head == NULL))
+	if (*head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	free(*head);
-	*head = NULL;
-	head = NULL;
 }
