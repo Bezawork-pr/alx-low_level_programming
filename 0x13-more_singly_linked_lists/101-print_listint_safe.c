c8716d39f4548b8d8d7d0b296e5c06862a02105a
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-17 04:29:40 -0700

    Print second loop start and exit with 98

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,14 +11,21 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 
-	while (head->next != NULL)
+	while (head)
 	{
-		if ((head->next) > head)
-			return (count);
-
-		printf("[%p] %d  \n", (void *)head, head->n);
-		count++;
-		head = head->next;
+		if ((head->next) < head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			count++;
+			head = head->next;
+		}
+		else
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			printf("->[%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
 	}
 	return (count);
 }
