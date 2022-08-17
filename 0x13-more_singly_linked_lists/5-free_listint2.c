1b35b698f237743a87266541276e6e41ab2ac4f2
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-17 00:14:11 -0700

    Rearrage code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,9 +16,9 @@ void free_listint2(listint_t **head)
 	temp = *head;
 	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	*head = NULL;
 	head = NULL;
