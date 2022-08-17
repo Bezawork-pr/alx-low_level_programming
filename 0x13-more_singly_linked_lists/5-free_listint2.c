34b9ffc648d91a8d8fa823d56669d78aeeb69463
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-17 00:31:44 -0700

    Free head and Equate head to zero

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,26 +1,24 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
- * free_listint2 - free list
+ * free_listint2 - frees a list
  *
- * @head: head of linked list
+ * @head: head of node
  *
- * Return: void
+ * Return void
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if ((*head == NULL) || (head == NULL))
-		exit(1);
+	if (*head == NULL)
+		return;
 
-	temp = *head;
 	while (*head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
-	*head = NULL;
-	head = NULL;
+	free(*head);
+	head = 0;
 }
