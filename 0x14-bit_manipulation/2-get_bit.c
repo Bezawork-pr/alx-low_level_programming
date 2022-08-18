b12bbcc34228442bf4f6410a411d1aa4a0956f64
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-18 04:46:04 -0700

    Get value using bitwise >> operator

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * get_bit - get value of bit at index given
+ *get value using >> bitwise operator
+ * @n: Takes number
+ *
+ * @index: index we check bit from
+ *
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int num, i = 31;
+
+	while (i >= 0)
+	{
+		num = n >> index;
+		return (num & 1);
+		i--;
+	}
+	return (-1);
+}
