742317194ce7bcf49ce7701a31400d586ddf6d8d
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-18 07:20:51 -0700

    Debug checker fail

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int value = 1 << index;
 	unsigned long int checksize;
 
-	checksize = sizeof(*n) * 8;
+	checksize = sizeof(unsigned long int) * 8;
 	*n = value | *n;
 		if (((*n >> index) == 1) && (index < checksize))
 			return (1);
