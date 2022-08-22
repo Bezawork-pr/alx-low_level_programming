a06608f82b335d03c4511b32bd581ed16c0acfe2
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-22 05:04:40 -0700

    Create function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * create_file - creates a function that creates a file
+ *
+ * @filename: Name of the file to create
+ *
+ * @text_content: NULL terminated string
+ * to write to the file created
+ *
+ * Return: 1 on SUCCESS
+ * -1 on FAILURE
+ * Ethier if File cannot be created
+ * OR CANNOT be written
+ * create a file with permision rw--------
+ * if the file exits DONOT change permision
+ * -1 if filename is NULL
+ *  if text_content is NULL create an emptyfile
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+	int length = 0;
+
+	while (text_content[length] != '\0')
+		length++;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	if (!text_content)
+	{
+		close(fd);
+		return (-1);
+	}
+	wr = write(fd, text_content, length);
+	if (wr == (-1))
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
