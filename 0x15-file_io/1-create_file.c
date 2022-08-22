241b0fd4bcc59bdf9299e708ffe8f05f93487009
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-22 05:23:43 -0700

    change position of return for !text_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,15 +21,15 @@ int create_file(const char *filename, char *text_content)
 	int fd, wr;
 	int length = 0;
 
+	if (!text_content)                                                                                                      
+        {                                                                                                                       
+                return (-1);                                                                                                    
+        } 	
 	while (text_content[length] != '\0')
 		length++;
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	umask(0);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-	if (!text_content)
 	{
 		close(fd);
 		return (-1);
