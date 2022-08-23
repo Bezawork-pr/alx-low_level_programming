d240dd7aa1fb7121e4d383528af43ac1f551513e
Bezawork Lindlöf <bezaworkalx@gmail.com>
2022-08-23 09:13:17 -0700

    Add exit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,20 +10,36 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd, rd, wr;
+	int buff_size = 18, fd, rd, wr;
+	char buff[18];
 
 	if (argc != 2)
-		return (1);
+	{
+		printf("ERROR ARGUMENT NUMBER WRONG\n");
+		exit(98);
+	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == (-1))
-		return (1);
-	rd = read(fd, argv[1], 18;
+	{
+		printf("Error: Can't open elf file\n");
+		exit(98);
+	}
+	rd = read(fd, buff, buff_size);
 	if (rd == (-1))
-		return (1);
-	wr = write(STDOUT_FILENO, argv[1], 18);
+	{
+		printf("Error: Can't read from file \n");
+		exit(98);
+	}
+	wr = write(STDOUT_FILENO, buff, rd);
 	if (wr == (-1))
-		return (1);
-	close(fd);
+	{
+		printf("Error: Can't write to stdout\n");
+		exit(98);
+	}
+	if (close(fd) == (-1))
+	{
+		printf("Error: Can't close fd FD_VALUE\n");
+		exit(98);
+	}
 	return (0);
-
 }
