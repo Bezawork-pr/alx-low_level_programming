dfce1666cbc6b8644ab06852453cd94812b2bfec
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-29 09:03:48 -0700

    use copy to iterate

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,11 +16,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int index = 0;
 	dlistint_t *previous;
 	dlistint_t *new;
+	dlistint_t *current;
 
 	new = malloc(sizeof(dlistint_t) * 100);
 	new->n = n;
 	new->prev = NULL;
 	new->next = NULL;
+	current = *h;
 	if (new == NULL)
 	{
 		free(new);
@@ -28,19 +30,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if ((idx == 0)  || (*h == NULL))
 		return (add_dnodeint(h, n));
-	while (*h)
+	while (current)
 	{
 		if (index == idx)
 			break;
 		index++;
-		previous = *h;
-		*h = (*h)->next;
+		previous = current;
+		current = current->next;
 	}
-	if ((*h)->next == NULL)
+	if (current->next == NULL)
 		return (add_dnodeint_end(h, n));
-	new->prev = (*h)->prev;
+	new->prev = current->prev;
 	new->next = previous->next;
 	previous->next = new;
-	(*h)->prev = new;
+	current->prev = new;
 	return (new);
 }
