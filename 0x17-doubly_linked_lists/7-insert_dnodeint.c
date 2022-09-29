ff9cd0a79e9ec730e9dbd3abe3eb3c1a3e168b80
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-29 09:26:32 -0700

    limit else statement

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	else
+	if ((idx != 0) && (*h == NULL))
 		return (NULL);
 	while (current)
 	{
