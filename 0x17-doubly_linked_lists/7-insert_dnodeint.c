15498cea10de94b483baf7a84da3d9e4d126a783
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-29 09:28:14 -0700

    Add braces

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new);
 		return (NULL);
 	}
-	if ((idx == 0)  || (*h == NULL) && (idx == 0))
+	if ((idx == 0)  || ((*h == NULL) && (idx == 0)))
 	{
 		return (add_dnodeint(h, n));
 	}
