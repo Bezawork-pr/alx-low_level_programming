438d20aabe13a95f9eebba4ddddc64db26330012
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-29 05:14:25 -0700

    Debug

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,5 +41,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = (*h)->prev;
 	new->next = previous->next;
 	previous->next = new;
+	(*h)->prev = new;
 	return (new);
 }
