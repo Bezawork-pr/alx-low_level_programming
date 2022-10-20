2b430b126d1fceb7d776d1074ca0f916febc27ea
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-20 04:03:07 -0700

    Implement djb2 algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+/**
+ * hash_djb2 - This function is the implementation of djb2 algoritm
+ *
+ * @str: string to be hashed
+ *
+ * Return: the hashed string unique number
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int i;
+	unsigned long int hash = 5381;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		hash = (hash * 33) + (int)str[i];
+	}
+	return (hash);
+}
