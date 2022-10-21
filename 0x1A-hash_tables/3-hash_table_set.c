bcbcd52f55aefd3ca9d8361435840693f5a47b96
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-21 01:56:34 -0700

    Fix betty error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,57 @@
 #include "hash_tables.h"
 /**
+ * free_node - frees node
+ *
+ * @node: to be freed
+ *
+ * Return: void
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+/**
+ * new_node_create - allocates memory and check malloc returns
+ *
+ * @ht: hash table
+ *
+ * @key: string
+ *
+ * @value: value to be stored
+ *
+ * Return: pointer to the newly created node
+ */
+hash_node_t *new_node_create(hash_table_t *ht,
+		const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if ((new == NULL) || ht == NULL
+			|| key == NULL  || value == NULL || strlen(key) == 0 ||
+			ht->size == 0)
+	{
+		free_node(new);
+		return (NULL);
+	}
+	new->key = strdup((char *)key);
+	if (new->key == NULL)
+	{
+		free_node(new);
+		return (NULL);
+	}
+	new->value = strdup((char *)value);
+	if (new->value == NULL)
+	{
+		free_node(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	return (new);
+}
+/**
  * hash_table_set - Add an element to the the hash table
  *
  * @ht: the hash table
@@ -16,50 +68,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *temp, *checker;
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
-	new = malloc(sizeof(hash_node_t));
-	if ((new == NULL) || ht == NULL  
-			|| key == NULL  || value == NULL || strlen(key) == 0 ||
-			ht->size == 0)
-	{
-		free(new);
-		return (0);
-	}
-	new->key = strdup((char *)key);
-	if (new->key == NULL)
-		return (0);
-	new->value = strdup((char *)value);
-	if (new->value == NULL)
-		return (0);
-	new->next = NULL;
+	new = new_node_create(ht, key, value);
 	if (ht->array[index] == NULL)
 		ht->array[index] = new;
 	else
 	{
-		/*if (strcmp(key, ht->array[index]->key) == 0)
+		checker = ht->array[index];
+		while (checker)
 		{
-			ht->array[index]->value = strdup((char *)value);
-			if (ht->array[index]->value == NULL)
-				return (0);
-			free(new);
-		}*/
-		/*if
-		{*/
-			checker = ht->array[index];
-			while (checker)
+			if (strcmp(key, checker->key) == 0)
 			{
-				if (strcmp(key, checker->key) == 0)
+				checker->value = strdup((char *)value);
+				if (checker->value == NULL)
 				{
-					checker->value = strdup((char *)value);
-					printf("%s", checker->value);
-					printf("Do u get here");
-					return (1);
+					free_node(new);
+					return (0);
 				}
-				checker = checker->next;
+				return (1);
 			}
-			temp = ht->array[index];
-			ht->array[index] = new;
-			new->next = temp;
-		/*}*/
+			checker = checker->next;
+		}
+		temp = ht->array[index];
+		ht->array[index] = new;
+		new->next = temp;
 	}
 	return (1);
 }
