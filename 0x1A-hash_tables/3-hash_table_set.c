1942f4ce33719a1e3f46c89c1e4307074b61a82b
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-20 23:15:07 -0700

    Add new node at the beginning in case of colluision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,6 +29,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new;
 		return (1);
 	}
+	else
+	{
+		ht->array[0] = new;
+		return (1);
+	}
 	free(new);
 	return (0);
 }
