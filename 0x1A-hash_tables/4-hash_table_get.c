ab9036c61a7a8987fe9bf395a18e3bec56921fa7
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-21 02:52:33 -0700

    Get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get value from the hashtable
+ *
+ * @ht: hash table
+ *
+ * @key: String
+ *
+ * Return: Value if found
+ * Otherwise NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *temp;
+
+	if (key == NULL || ht->array[index] == NULL || strlen(key) == 0)
+		return (NULL);
+	if (index < ht->size)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			return (ht->array[index]->value);
+		temp = ht->array[index];
+		while (temp)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
+	}
+	return (NULL);
+}
