0bb4c5cd085fab2ef2deea37dbf73ad92ea57432
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-21 00:29:20 -0700

    Debug

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else 
 		if (strcmp(key, ht->array[index]->key) == 0)
 		{
-			ht->array[index]->value = strdup(value);
+			ht->array[index]->value = strdup((char *)value);
+			free(new);
 			return (1);
 		}
 		else
