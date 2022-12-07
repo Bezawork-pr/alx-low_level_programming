9bd7bd75c36568ab2ae9b6860d089829cffa1864
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-07 03:19:52 -0800

    Calculate parameter of each row separately

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,10 +7,21 @@ gets perimeter
 
 def island_perimeter(grid):
     """Gets perimeter"""
-    sum = 0
+    length = 0
+    flag = 0
+    my_array = []
+    my_sum = 0
     for row in grid:
+        flag = 0
         for elem in row:
             if elem == 1:
-                sum += 1
-    sum += 1
-    return sum * 2
+                length += 1
+                flag += 1
+        my_array.append(flag)
+    for i in my_array:
+        if i != 1 and i != 0:
+            my_sum += i + 1
+        elif i == 1:
+            my_sum += 1
+
+    return my_sum * 2
