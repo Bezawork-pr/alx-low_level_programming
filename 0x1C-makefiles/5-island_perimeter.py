9ed3dab82413c902587dcf6578130ae186be9b5e
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-07 03:36:03 -0800

    Edit to code to deal with different scenarios

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,17 +7,23 @@ gets perimeter
 
 def island_perimeter(grid):
     """Gets perimeter"""
-    length = 0
-    flag = 0
+    count = 0
     my_array = []
     my_sum = 0
+    flag = 0
     for row in grid:
+        count = 0
         flag = 0
         for elem in row:
             if elem == 1:
-                length += 1
-                flag += 1
-        my_array.append(flag)
+                flag = 1
+                count += 1
+            elif elem == 0 and flag == 1:
+                flag = 0
+                my_array.append(count)
+                count = 0
+                flag = 0
+        my_array.append(count)
     for i in my_array:
         if i != 1 and i != 0:
             my_sum += i + 1
