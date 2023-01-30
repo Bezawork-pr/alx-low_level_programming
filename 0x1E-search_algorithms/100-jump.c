1aec28732a5b52f3907db37b7c69075524fb4bb9
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-30 00:56:20 -0800

    Return -1 when size == 0

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,7 +21,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t jump = round(sqrt(size)), i, start, end;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 	for (i = 0; i < size; i = i + jump)
 	{
